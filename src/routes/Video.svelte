a4f143372bc14ad461a1811f459a3c61d988400a
Andrew G. Neumann <agneum14@gmail.com>
2026-02-18 02:09:01 -0500

    add basic dynamic video component

---
+++ src/routes/Video.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { PUBLIC_FS_URL } from '$env/static/public';
+
+	export let videoName: string;
+
+	const videoSrc = `${PUBLIC_FS_URL}/videos/${videoName}.mp4`;
+</script>
+
+<video autoplay loop muted>
+	<source src={videoSrc} type="video/mp4" />
+	Video element not supported :(
+</video>
