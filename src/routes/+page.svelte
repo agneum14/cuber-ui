3722be2901f040371a3538f29dcc32e518d1e3fb
Andrew G. Neumann <agneum14@gmail.com>
2026-02-19 00:07:30 -0500

    handle video and shader websocket msgs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,40 @@
 <script lang="ts">
 	import Video from './Video.svelte';
+	import { onMount } from 'svelte';
+	import { PUBLIC_WS_URL } from '$env/static/public';
+	import { WebSocketMsgSchema, type WebSocketMsg } from '$lib/schemas/websocketSchema';
 
 	let videoName = 'anime_twerk';
 	let shaderName = '';
 
-	let newVideoName = '';
-	let newShaderName = '';
-	function handleVideo() {
-		videoName = newVideoName;
+	function handleMsg(msg: WebSocketMsg) {
+		console.log('handling', msg);
+		if (msg.tool == 'video') {
+			videoName = msg.payload.name;
+		} else if (msg.tool == 'shader') {
+			shaderName = msg.payload.name;
+		}
 	}
-	function handleShader() {
-		shaderName = newShaderName;
-	}
+
+	onMount(() => {
+		const socket = new WebSocket(PUBLIC_WS_URL);
+
+		socket.onopen = () => {
+			console.log('websocket connected');
+		};
+		socket.onclose = () => {
+			console.log('websocket closed');
+		};
+
+		socket.onmessage = (event) => {
+			const data = JSON.parse(event.data);
+			const result = WebSocketMsgSchema.safeParse(data);
+			if (result.success) {
+				const msg: WebSocketMsg = result.data;
+				handleMsg(msg);
+			}
+		};
+	});
 </script>
 
-<form on:submit={handleVideo}>
-	<input type="text" bind:value={newVideoName} />
-	<button type="submit">Submit</button>
-</form>
-<form on:submit={handleShader}>
-	<input type="text" bind:value={newShaderName} />
-	<button type="submit">Submit</button>
-</form>
 <Video {videoName} {shaderName} />
